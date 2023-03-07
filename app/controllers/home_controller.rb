92b8a16d0a8db76b284a0fd9d54505b0ae12b9ab
Mateusz Wieczorek <mateusz.wu@gmail.com>
2023-03-07 11:47:05 +0100

    allow unlogged user to enter home and property show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
   def show
     @properties = Property.all
   end
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 class PropertiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
   before_action :set_property, only: %i[show edit update destroy]
 
   # GET /properties or /properties.json
